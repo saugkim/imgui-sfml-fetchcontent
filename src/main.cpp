d6482b8ebdc513e7aaeed638af0f1d0ade8ae8e0
Kim <ugkimm@naver.com>
2020-11-01 15:57:51 +0200

    resourses folder added and modified cmake to include resource folder and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
+#include <SFML/Graphics.hpp>
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
@@ -13,6 +14,18 @@ int main() {
 
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+	
+	sf::Texture texture1, texture2, texture_inductor, texture_cvs;
+    if ( !texture1.loadFromFile("resources/resistor.png") ||
+		 !texture2.loadFromFile("resources/capacitor.png") ||
+		 !texture_inductor.loadFromFile("resources/inductor.png") || 
+		 !texture_cvs.loadFromFile("resources/power.png") )
+        return EXIT_FAILURE;
+    sf::Sprite sprite1(texture1);
+	sf::Sprite sprite4(texture_cvs);
+    sf::Sprite sprite2(texture2);
+    //sprite1.setOrigin(sf::Vector2f(texture1.getSize()) / 2.f);
+    //sprite1.setPosition(sprite1.getOrigin());
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -27,8 +40,14 @@ int main() {
 
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
+        ImGui::Begin("MENU");
+		ImGui::ImageButton(sprite1);
+		ImGui::ImageButton(texture2);
+		ImGui::ImageButton(texture_inductor);
+		ImGui::ImageButton(sprite4);
+		ImGui::Button("Simulate");
+        ImGui::Button("SAVE");
+		ImGui::Button("LOAD");
         ImGui::End();
 
         window.clear();
