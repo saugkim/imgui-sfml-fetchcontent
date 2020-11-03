bff51801315734a49a79b75fd119fb1112f01c33
Kim <ugkimm@naver.com>
2020-11-03 16:56:06 +0200

    instantiate image object when click

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "imgui.h"
 
 #include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
@@ -22,40 +23,144 @@ int main() {
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 	
+	//Event object holding all events
+    sf::Event event;
+
+    //States for button/events
+    bool mouseClicked = false;
+    bool mouseInsideRect = false;
+    bool dragging = false ;
+
+    sf::Vector2f mouseRectOffset ;
+
+    //Variables
+    int mouseX = 0;
+    int mouseY = 0;
+	
+	
 	sf::Texture texture_resistor, texture_capacitor, texture_inductor, texture_cvs;
     if ( !texture_resistor.loadFromFile("resources/resistor.png") ||
 		 !texture_capacitor.loadFromFile("resources/capacitor.png") ||
 		 !texture_inductor.loadFromFile("resources/inductor.png") || 
 		 !texture_cvs.loadFromFile("resources/power.png") )
         return EXIT_FAILURE;
-    sf::Sprite sprite1(texture_resistor);
-	sf::Sprite sprite2(texture_capacitor);
-	sf::Sprite sprite3(texture_inductor);
-	sf::Sprite sprite4(texture_cvs);
+		
+    //sf::Sprite sprite1(texture_resistor);
+	//sf::Sprite sprite2(texture_capacitor);
+	//sf::Sprite sprite3(texture_inductor);
+	//sf::Sprite sprite4(texture_cvs);
+	std::vector<sf::Sprite> sprites;
+		
+	bool element_clicked = false;
+	bool removing_element = false;
+	int selected_element = -1;
     
     sf::Clock deltaClock;
     while (window.isOpen()) {
-        sf::Event event;
+		
         while (window.pollEvent(event)) {
             ImGui::SFML::ProcessEvent(event);
+			
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+            {
+                mouseClicked = true;
+
+				if (element_clicked)
+				{
+                    dragging = true;
+					std::cout << "dragging at creating " << std::endl;
+                }
+				
+				for (int i=0; i<sprites.size(); i++) {
+					if ( sprites[i].getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) ) {
+						selected_element = i ;
+						dragging = true ;
+						std::cout << "dragging after created ? " << std::endl;
+					}
+				}
+            }
+			
+			for (int i=0; i<sprites.size(); i++) {
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Delete)) {		
+					if ( sprites[i].getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) ) {
+						std::cout << "i am in " << i << std::endl;
+						selected_element = i ;
+						std::cout << "Removing" << std::endl;
+						removing_element = true;
+						break;
+					}
+				}
+			}
+					
+					
+            if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
+
+				std::cout << "mouse released" << std::endl;
+                mouseClicked = false;
+                dragging = false ;
+				element_clicked = false;
+            }
+
+
+            if (event.type == sf::Event::MouseMoved) {
+				
+                mouseX = event.mouseMove.x;
+                mouseY = event.mouseMove.y;
+            }
+			
 
             if (event.type == sf::Event::Closed) {
+				
                 window.close();
             }
         }
 			
+			
 		static const auto size = ImVec2(150, 50);
-
         ImGui::SFML::Update(window, deltaClock.restart());
 		
 		ImGui::Begin("CIRCUIT ELEMENTS");
-		ImGui::ImageButton(sprite1);
-		ImGui::ImageButton(sprite2);
-		ImGui::ImageButton(texture_inductor);
-		ImGui::ImageButton(sprite4);
+		if (ImGui::ImageButton(texture_resistor) ){
+			
+			selected_element = sprites.size();
+			sf::Sprite sprite(texture_resistor);
+			sprite.setPosition(event.mouseMove.x, event.mouseMove.y);
+			sprites.push_back(sprite);
+			element_clicked = true;
+			
+		};
+		
+		if (ImGui::ImageButton(texture_capacitor) ){
+			
+			selected_element = sprites.size();
+			sf::Sprite sprite(texture_capacitor);
+			sprite.setPosition(event.mouseMove.x, event.mouseMove.y);
+			sprites.push_back(sprite);
+			element_clicked = true;
+		}
+		
+		if (ImGui::ImageButton(texture_inductor) ){
+			
+			selected_element = sprites.size();
+			sf::Sprite sprite(texture_inductor);
+			sprite.setPosition(event.mouseMove.x, event.mouseMove.y);
+			sprites.push_back(sprite);
+			element_clicked = true;
+		}
+		
+		if (ImGui::ImageButton(texture_cvs) ){
+		
+			selected_element = sprites.size();
+			sf::Sprite sprite(texture_cvs);
+			sprite.setPosition(event.mouseMove.x, event.mouseMove.y);
+			sprites.push_back(sprite);
+			element_clicked = true;
+		}
+		
 		ImGui::Spacing();
 		ImGui::Button("SIMULATE", size);
 		ImGui::End();
+		
 		
 		
 		static const auto size1 = ImVec2(100, 30);
@@ -75,7 +180,6 @@ int main() {
 			std::string f = path + fname + ending;
 		
 			saveAsText(f);
-			shape.setFillColor(sf::Color::Red);
 		}
 		
 		ImGui::Spacing();
@@ -91,7 +195,6 @@ int main() {
 			std::string f = path + a + b;
 			
 			loadFromFile(f);
-			shape.setFillColor(sf::Color::Green);
 		}	
         ImGui::End();
 	
@@ -144,8 +247,22 @@ int main() {
 		ImGui::End();
 		
 		
-        window.clear();
-        window.draw(shape);
+		
+		window.clear();
+		if (dragging == true) {
+			sprites[selected_element].setPosition(mouseX, mouseY);
+		}
+		
+		if (removing_element == true) {
+			sprites.erase(sprites.begin() + selected_element);
+			removing_element = false;
+		}
+		
+		for (unsigned int i = 0; i < sprites.size() ; i++) {
+			
+			window.draw(sprites[i]);
+		}
+		
         ImGui::SFML::Render(window);
         window.display();
     }
@@ -154,3 +271,4 @@ int main() {
 
     return 0;
 }
+
